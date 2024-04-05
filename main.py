8462ab541e994a23573d1080527a264273650756
Juan Manuel Schillaci <jmschillaci@gmail.com>
2024-04-04 23:31:35 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import time
+
+import flet as ft
+
+
+resultado = ft.Text("B")
+
+def old_main(page: ft.Page):
+    # add/update controls on Page
+    page.add(
+    ft.Row(controls=[
+        t:=ft.Text("A"),
+        resultado,
+        ft.Text("C")
+    ]))
+    page.add(
+     ft.Row(controls=[
+        ft.TextField(label="Your name"),
+        ft.ElevatedButton(text="Say my name!", on_click=button_clicked)
+    ])
+    )
+    for i in range(10):
+        t.value = f"Step {i}"
+        page.update()
+        time.sleep(1)
+
+def button_clicked(e):
+    resultado.value='Click'
+
+def norefs_main(page):
+
+    first_name = ft.TextField(label="First name", autofocus=True)
+    last_name = ft.TextField(label="Last name")
+    greetings = ft.Column()
+
+    def btn_click(e):
+        greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
+        first_name.value = ""
+        last_name.value = ""
+        page.update()
+        first_name.focus()
+
+    page.add(
+        first_name,
+        last_name,
+        ft.ElevatedButton("Say hello!", on_click=btn_click),
+        greetings,
+    )
+
+
+def refs_main(page):
+
+    first_name = ft.Ref[ft.TextField]()
+    last_name = ft.Ref[ft.TextField]()
+    greetings = ft.Ref[ft.Column]()
+
+    def btn_click(e):
+        greetings.current.controls.append(
+            ft.Text(f"Hello, {first_name.current.value} {last_name.current.value}!")
+        )
+        first_name.current.value = ""
+        last_name.current.value = ""
+        page.update()
+        first_name.current.focus()
+
+    page.add(
+        ft.TextField(ref=first_name, label="First name", autofocus=True),
+        ft.TextField(ref=last_name, label="Last name"),
+        ft.ElevatedButton("Say hello!", on_click=btn_click),
+        ft.Column(ref=greetings),
+    )
+
+def counter_main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align="right", width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+def main(page: ft.Page):
+    def button_clicked(e):
+        output_text.value = f"Dropdown value is:  {color_dropdown.value}"
+        page.update()
+
+    output_text = ft.Text()
+    submit_btn = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+    color_dropdown = ft.Dropdown(
+        width=100,
+        options=[
+            ft.dropdown.Option("Red"),
+            ft.dropdown.Option("Green"),
+            ft.dropdown.Option("Blue"),
+        ],
+    )
+    page.add(color_dropdown, submit_btn, output_text)
+
+ft.app(target=main)
+#ft.app(target=main)
+#ft.app(port=8550, target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+
